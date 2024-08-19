e67d7ffff21086b97472f6e198c4d14b4ba475b7
Arshnoor kirmani <study.arshnoorkirmani@gmail.com>
2024-08-19 13:39:04 +0530

    complete cites section and create setting section and his units btn script are complete

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-document.querySelector(".cites").addEventListener("click", () => {
-  document.querySelector(".cites-seciton").style.display = "grid";
-  document.querySelector(".weather-box").style.display = "none";
-});
-document.querySelector(".weather").addEventListener("click", () => {
-  document.querySelector(".cites-seciton").style.display = "none";
-  document.querySelector(".weather-box").style.display = "grid";
+let weatherBody, citesBody, settingBody, settingUnitsBtns;
+// ====================================================== NavBar Button's Logic============================================================
+weatherBody = document.querySelector(".weather-box");
+citesBody = document.querySelector(".cites-seciton");
+settingBody = document.querySelector(".setting-section");
+function weathershow() {
+  weatherBody.classList.remove("displaynone");
+  citesBody.classList.add("displaynone");
+  settingBody.classList.add("displaynone");
+  document.querySelector(".right").classList.remove("displaynone");
+}
+function citesshow() {
+  citesBody.classList.remove("displaynone");
+  weatherBody.classList.add("displaynone");
+  settingBody.classList.add("displaynone");
+  document.querySelector(".right").classList.remove("displaynone");
+}
+function settingshow() {
+  settingBody.classList.remove("displaynone");
+  document.querySelector(".right").classList.add("displaynone");
+}
+// ====================================================== Setting Uints Button's Logic======================================================
+settingUnitsBtns = document.querySelectorAll(".menus");
+settingUnitsBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document.querySelectorAll(`.${e.target.classList[1]}`).forEach((e) => {
+      e.classList.remove("selected");
+    });
+    e.target.classList.add("selected");
+  });
 });
